44ec3c80766c1d50615e8c6d81ea0f9238f62c89
Lee Sung Guk <jin3137@gmail.com>
2019-04-06 10:51:53 +0900

    테스트 코드 수정

--- MembershipManagemnetSystem/tests/test_sign_up.py
+++ MembershipManagemnetSystem/tests/test_sign_up.py
@@ -5,23 +5,37 @@ import pytest
 from datetime import datetime, timedelta
 from jwt import InvalidSignatureError
 
-# 회원가입
+
+# 회원가입 토큰 발급
 def sign_up(email: str, pw: str) -> str:
     input_data = {
         "email": email,
         "pw": pw
     }
-    key = 'secret'  #
-    jwt_data = jwt.encode(input_data, key, algorithm='HS256')
+    key = 'secret'
 
-    return jwt_data
+    # 테스트용
+    # jwt_data = jwt.encode(input_data, key, algorithm='HS256')
 
-# 토큰생성
+    now = datetime.now()
+    exp = timedelta(seconds=2)
+
+    payload = {
+        'iss': email,
+        'exp': now + exp
+    }
+
+    jwt_token = jwt.encode(payload, key, algorithm='HS256')
+
+    return jwt_token
+
+
+# 토큰생성 (만료 정보만)
 def make_jwt_token(email: str) -> str:
     # 토큰 정보 입력
     # now: 현재 시간, exp: 만료 시간 정보
     now = datetime.now()
-    exp = timedelta(seconds=2)
+    exp = timedelta(minutes=60)
     print("now: {}, exp: {}".format(now, exp))
 
     return jwt.encode({'exp': now + exp}, 'scret')
